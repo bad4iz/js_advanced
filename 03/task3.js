05545930ffa190bbc4e01ed42401ff4fd27de336
Евгений Плотников <bad4iz@gmail.com>
2017-03-31 17:07:10 +0400

    3.3

--- 03/task3.js
+++ 03/task3.js
@@ -1,7 +1,8 @@
 var task3 = document.getElementById('task3');
 var resultTest3 = document.getElementById('resultTest3');
 var one = '', two = '';
+var reg = /\+-*\//;
 task3.onchange   = function () {
-
+    console.log(task3.value.search(/[\+\-\*]/));
     document.getElementById('resultTest3').textContent = task3.value;
 };
