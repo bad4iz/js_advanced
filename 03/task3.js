f8099d80e9fd251146859467acf4ccd38be70b2b
Евгений Плотников <bad4iz@gmail.com>
2017-04-10 13:20:39 +0400

    3.3

--- 03/task3.js
+++ 03/task3.js
@@ -1,14 +1,22 @@
 var task3 = document.getElementById('task3');
 var resultTest3 = document.getElementById('resultTest3');
-var arr = [];
 var ind;
 var val;
-var reg = /\+-*\//;
+var reg = /[\+\-\*\/]/g;
 task3.onchange = function () {
     val = task3.value;
-    ind = val.search(/[\+\-\*]/)
-    if (~ind){
-        console.log(new Number(val.slice(0,ind)) + val.slice(ind, ind + 1) + new Number(val.slice(ind + 1) );
+
+    var operator = val.match(reg);
+    var itog = 0;
+    if( 1 == operator.length ){
+        var arr  = val.split(operator);
+        var a = arr[0];
+        var b = arr[1];
+
+        if (operator == '-') itog = +a - +b;
+        if (operator == '+') itog = +a + +b;
+        if (operator == '*') itog = +a * +b;
+        if (operator == '/') itog = +a / +b;
+        resultTest3.textContent = itog;
     }
-    document.getElementById('resultTest3').textContent = task3.value;
 };
